b5f48fea9b315a5034105f75bcc418d69fbdc334
xuby <diazxavier27@gmail.com>
2025-09-06 00:07:08 -0400

    ✨ feat: add functionality to change form orientation in preview and improve submit button

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -1,6 +1,7 @@
 import { AntdSchemaForm } from '@xubylele/antd-form-generator';
-import { Card, Col, Row, Space } from 'antd';
+import { Button, Card, Col, Row, Space } from 'antd';
 import { JsonEditor } from 'json-edit-react';
+import { useState } from 'react';
 import type { PreviewProps } from '../types';
 
 export function Preview({
@@ -10,21 +11,35 @@ export function Preview({
   submited,
   setSubmited,
 }: PreviewProps) {
+  const [orientation, setOrientation] = useState<'vertical' | 'horizontal'>('vertical');
 
   return (
     <Row gutter={16}>
       <Col span={24}>
         <Space direction="vertical" size={16} style={{ width: '100%' }}>
           <Card title="Preview">
-            {schema && uiSchema && (
-              <AntdSchemaForm
-                orientation='vertical'
-                schema={schema}
-                uiSchema={uiSchema}
-                initialValues={initialValues}
-                onFinish={(values) => setSubmited(values)}
-              />
-            )}
+            <Space direction="vertical" size={16}>
+              <Button
+                onClick={() => setOrientation(orientation === 'vertical' ? 'horizontal' : 'vertical')}
+                type="default"
+              >
+                Change Orientation
+              </Button>
+              {schema && uiSchema && (
+                <AntdSchemaForm
+                  orientation={orientation || 'vertical'}
+                  schema={schema}
+                  uiSchema={uiSchema}
+                  initialValues={initialValues}
+                  onFinish={(values) => setSubmited(values)}
+                  submitButtonProps={{
+                    type: 'primary',
+                    size: 'large',
+                    title: 'Submit Form',
+                  }}
+                />
+              )}
+            </Space>
           </Card>
           <Card title="Submited Values">
             <JsonEditor
